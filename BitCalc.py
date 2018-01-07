dd936618f652980fff5d7511c68fe111e429564e
Adalberto Caldeira Brant Filho <adalbertobrant@gmail.com>
2018-01-07 18:47:49 -0200

    Modificações para api uol
    Foram feitas modificações para utilização da Seguinte Api http://economia.awesomeapi.com.br/ que mostra suas cotações de 10 em 10 minutos segundo o desenvolvedor

--- BitCalc.py
+++ BitCalc.py
@@ -14,28 +14,42 @@ from bs4 import BeautifulSoup
 
 # Faz a captura da página para a variável 'pagina'
 
-pagina = requests.get('https://www.coingecko.com/en/coins/bitcoin')
+pagina = requests.get('http://economia.awesomeapi.com.br/BTC-BRL/1?format=xml')
+dolarReal = requests.get('http://economia.awesomeapi.com.br/USD-BRL/1?format=xml')
+
+
 
 # Obejeto do BeautifulSoup 
 
 soup = BeautifulSoup(pagina.text, 'html.parser')
+soup1 = BeautifulSoup(dolarReal.text,'html.parser')
+
+
+
+# Imprime Soup1 comando prettify()
+
+#print(soup1.prettify())
+
+#print(list(soup1.children))
+
 
 # Extrai do site da informação desejada nesse caso o valor do BitCoin no dia
 # 
 
-valor = soup.find(class_='coin-value')
+valor = soup.find_all('high')
+vdolarReal = soup1.find_all('high')
 
 # imprime a variável valor 
 
 # mostra o tipo da variável fins didáticos
-# print(type(valor))
+#print(type(vdolarReal))
 
 # dentro da variável valor podemos ver que existem diversos outros indicadores
 # devemos extrair o indicador desejado
 
 # pega e imprime o valor do BTC
 
-precoBTC = re.findall(r'\$[0-9,.]+',valor.text)
+#precoBTC = re.findall(r'\$[0-9,.]+',valor.text)
 
 # transforma precoBTC em string
 
@@ -44,7 +58,9 @@ precoBTC = re.findall(r'\$[0-9,.]+',valor.text)
 # print(type(precoBTC))
 
 # Transforma a lista em string
-BTC = str(precoBTC)
+BTC = str(valor)
+Dolar = str(vdolarReal)
+
 
 # Mostra o tipo de variável
 
@@ -54,17 +70,23 @@ BTC = str(precoBTC)
 # primeiro remoção dos colchetes( [] ), depois cifrão ($), depois a vírgula(,) e por fim
 # as aspas simples (') 
 
+
+
+Dolar = str(Dolar).strip('[]')
+
+Dolar = str(Dolar).strip('<high>')
+
+Dolar = str(Dolar).strip('</')
+
 BTC = str(BTC).strip('[]')
+BTC = str(BTC).strip('<high>')
+BTC = str(BTC).strip('</')
 
-BTC = BTC.replace('$',"")
-
-BTC = BTC.replace(',',"")
-
-BTC = BTC.strip("'")
 
 #  transforma a string em float
 
 teste = ast.literal_eval(BTC)
+teste1 = ast.literal_eval(Dolar)
 
 
 # Mostra o tipo da variável teste
@@ -74,7 +96,20 @@ teste = ast.literal_eval(BTC)
 
 #print(teste)
 
-# Variável BTC agora é to tipo float por receber a variável teste
-BTC=teste
+# Variável BTC e Dolar  agora é to tipo float por receber a variável teste
+BTC = teste
+Dolar = teste1
 
+print (" \nPrograma para o Cálculo do Valor de Bitcoin em Dolar do dia\n ")
 
+print (" 1 BTC Vale = R$",BTC," reais")
+
+print (" 1 Dolar Comercial Vale = R$ ",Dolar,"\n")
+
+usrBTC = input("Entre com o valor desejado em BTC = ")
+
+usrBTC = float(usrBTC)
+
+calculo = usrBTC * BTC
+
+print(" \nO valor em reais é = R$  ",calculo)
